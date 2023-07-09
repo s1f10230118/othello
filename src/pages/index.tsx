a1d50220ae203dd536cd33fd8719a5d4b53dc585
s1f10230118 <136315415+s1f10230118@users.noreply.github.com>
2023-07-09 18:22:49 +0900

    日本語

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -2,7 +2,7 @@ import { useState } from 'react';
 import styles from './index.module.css';
 
 const Home = () => {
-  const[turnColor,setTurnColor]=useState(1)
+  const [turnColor, setTurnColor] = useState(1);
   const [board, setBoard] = useState([
     [0, 0, 0, 0, 0, 0, 0, 0],
     [0, 0, 0, 0, 0, 0, 0, 0],
@@ -13,26 +13,30 @@ const Home = () => {
     [0, 0, 0, 0, 0, 0, 0, 0],
     [0, 0, 0, 0, 0, 0, 0, 0],
   ]);
-  const onClick=(x:number,y:number)=>{
-    console.log(x,y)
-    const newBoard:number[][]=
-    JSON.parse(JSON.stringify(board))
-    newBoard[y][x]=turnColor
-    setBoard(newBoard)
-    if (turnColor===1){
-      setTurnColor(2)
-    }else{
-      setTurnColor(1)
+  const onClick = (x: number, y: number) => {
+    console.log(x, y);
+    const newBoard: number[][] = JSON.parse(JSON.stringify(board));
+    newBoard[y][x] = turnColor;
+    setBoard(newBoard);
+    if (turnColor === 1) {
+      setTurnColor(2);
+    } else {
+      setTurnColor(1);
     }
-  }
+  };
 
   return (
     <div className={styles.container}>
       <div className={styles.board}>
         {board.map((row, y) =>
           row.map((color, x) => (
-            <div className={styles.cell} key={`${x}-${y}`} onClick={()=>onClick(x,y)}>
-              { color !==0&& <div className={styles.stone} style={{background:color===1?'#000':'#fff'}} /> }
+            <div className={styles.cell} key={`${x}-${y}`} onClick={() => onClick(x, y)}>
+              {color !== 0 && (
+                <div
+                  className={styles.stone}
+                  style={{ background: color === 1 ? '#000' : '#fff' }}
+                />
+              )}
             </div>
           ))
         )}
